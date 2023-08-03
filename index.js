9d489a98008ea81b43debb468605d4eda373488c
RaghavendraPatel <raghavpatel2808@hotmail.com>
2023-08-03 19:12:11 +0530

    updated index

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const port = 8000;
-
 const app = express();
+
+const db = require("./config/mongoose");
+const bodyParser = require("body-parser");
 
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
+
+app.use("/", require("./routes"));
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(`Error creating server: ${err}`);
+    return;
+  }
+  console.log(`Server running on Port: ${port}`);
+});
